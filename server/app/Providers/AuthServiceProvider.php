cbeddc9d10fb5e40c49250aa5c1fd88205e3b553
NuwanthaPasindhu <hnpkdias@gmail.com>
2022-06-20 10:11:17 +0530

    File Structure

--- server/app/Providers/AuthServiceProvider.php
+++ server/app/Providers/AuthServiceProvider.php
@@ -2,9 +2,8 @@
 
 namespace App\Providers;
 
-use Laravel\Passport\Passport;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,7 +24,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        Passport::routes();
+
         //
     }
 }
