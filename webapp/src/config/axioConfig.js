4b6fb880ccd2655d303eedadf6f62ebddc8d3b4a
NuwanthaPasindhu <hnpkdias@gmail.com>
2022-08-04 16:09:41 +0530

    change to production url

--- webapp/src/config/axioConfig.js
+++ webapp/src/config/axioConfig.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
-// axios.defaults.baseURL ='https://testserver.virtuelsolutions.com/api/';
+axios.defaults.baseURL ='https://testserver.virtuelsolutions.com/api/';
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
